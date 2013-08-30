43223f12f38ba81dbb4995e082b5de4043a27026
unknown <daruttka@RUTTKA-LAPTOP.redmond.corp.microsoft.com>
2013-08-30 09:34:58 -0400

    Initial commit from template

---
+++ StudioBash.Package/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace TheDevStop.StudioBash_Package
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidStudioBash =    0x101;
+
+    };
+}
---
+++ StudioBash.Package/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StudioBash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The DevStop")]
+[assembly: AssemblyProduct("StudioBash")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("StudioBash.Package_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000147cbd8f9233a18e1985ce4c3f97ef78701e5562f79f29a25ef89942bb661153fa3a27f3529a67db0775bee3bfd5079aff95c622f5b06731eccd641b5aceb9ca747bec3aed137d7dbfdbcf84ac9e0880ac7300feadf9f189814fed8881f9355157cfdf2e4c5015009033fad768f188cb539b34ba8b12ae2f9173d0b655b34ba")]
+[assembly: InternalsVisibleTo("StudioBash.Package_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000147cbd8f9233a18e1985ce4c3f97ef78701e5562f79f29a25ef89942bb661153fa3a27f3529a67db0775bee3bfd5079aff95c622f5b06731eccd641b5aceb9ca747bec3aed137d7dbfdbcf84ac9e0880ac7300feadf9f189814fed8881f9355157cfdf2e4c5015009033fad768f188cb539b34ba8b12ae2f9173d0b655b34ba")]
